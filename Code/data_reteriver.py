3d8bec533c3641effb8841716ab0371c01be644d
prince68charles <simonlavers3@outlook.com>
2026-04-29 19:50:12 -0300

    Added skelton for data reterival

---
+++ Code/data_reteriver.py
@@ -0,0 +1,31 @@
+from load_repo import load_repoistory
+import pandas as pd
+import datetime
+
+
+"""This file will retreive data for scores"""
+
+
+def retreieve_contributors(repo: list[dict]) -> list: 
+
+
+    """This will reterive a list of contriubuters for each repoistory"""
+
+
+
+def retreive_last_commit(repo: list[dict]) -> list:
+
+    """This will reterive a list of commits for each repo"""
+
+
+
+def retereive_star_growth(repo: list[dict]) -> int:
+
+     """This will reterive the number of stars in the last 90 days"""
+
+
+def recency_score(repo:list[dict]) -> datetime:
+
+    """Will reterive the date of the last commit"""
+
+
