4597a309f97c8f4ecd2c5a28479394abf7c1106e
prince68charles <simonlavers3@outlook.com>
2026-04-30 09:13:51 -0300

    Fixed loading format

--- Code/load_repo.py
+++ Code/load_repo.py
@@ -1,6 +1,8 @@
 """In this file we will load the repos and sucsefully conver them into readable data"""
 import requests
 import json
+
+
 class InvalidRepositoryError:
 
 
@@ -12,25 +14,53 @@ class InvalidRepositoryError:
 
 
 
-def load_repoistory(url, headers):
+def load_repoistory(url, headers, owner, repo):
 
     """Here we will load the repoistory for storing and convert it into json"""
 
-    response = requests.get(url, headers)
+    metadata =  requests.get(url, headers).json()
+
+
+    #Gather languages
+    lang_url = f"{url}/languages"
+    languages = requests.get(lang_url, headers).json()
+
+
+    #Gather full file
+    branch = metadata.get('default_branhc', 'main')
+    tree_url = f"https://api.github.com/repos/{owner}/{repo}/git/trees/{branch}?recurisve=1"
+    tree_data = requests.get(tree_url, headers).json()
+
+
+    full_json_format = {
+
+        "repo-name": metadata.get("name"),
+        "desciription": metadata.get("description"),
+        "visibility": metadata.get("visibility"),
+        "stats": {
+
+            "stars": metadata.get("stargazers_count"),
+            "forks": metadata.get("forks_count"),
+            "open_issues": metadata.get("open_issues_count")
+
+        },
+
+        "languages": languages,
+        "file-structure": [{"path": item["path"], "type": item["type"], "size": item.get("size")}
+            for item in tree_data.get("tree", [])
+        ]
+
+    }
+
 
     try:
 
-        if response.status_code == 200:
-        
-            #Here is the github repo
-            response = response.json()
+        with open('loaded_repo.json', 'w') as f:
 
-            with open("repos.json", "w") as f:
+            json.dump(full_json_format, f, indent= 4)
 
-                json.dump(response, f, indent = 4)
-        
-    except InvalidRepositoryError:
-
-        raise("Invalid repo, check again")
+            print("Repo found.")
     
+    except FileNotFoundError:
 
+        raise ("Could not find repoistory") 
